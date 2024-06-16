b8090f86d6f21d4d972272dd4520413849a84295
Denis Andronov <denisunderonov@gmail.com>
2024-06-17 02:58:38 +0300

    updateURLS

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 import pkg from 'pg'
+import { bdURL } from './var.js';
 const { Pool } = pkg;
 
 const pool = new Pool({
     user: 'postgres', 
-    host: 'localhost', 
+    host: `${bdURL}`, 
     database: 'courseDB', 
     password: 'Denimz13', 
     port: 5432,
---
+++ var.js
@@ -0,0 +1,1 @@
+export const bdURL = `localhost`
